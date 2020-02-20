34777fb2944c565010d1f097e10c64dd885697a6
magikitty <elina.satola@gmail.com>
2020-02-20 20:54:22 +0200

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+# Messages
+MESSAGE_ANIMAL_ATTACK = "The mutant animal attacks with"
+MESSAGE_ENTER_NAME = "Enter your name: "
+MESSAGE_PLAYER_SURVIVED_TRUE = "You survived the mutant animal attack!"
+MESSAGE_PLAYER_SURVIVED_FALSE = "Oh no, you did not survive the mutant animal attack!"
+
+# Mutant animal names
+ANIMAL_1 = "Moophant"
