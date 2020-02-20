b70e11b2c2ea8c55b32ed6b81736395935b6474e
magikitty <elina.satola@gmail.com>
2020-02-20 20:55:05 +0200

    Replace strings with constants

--- survival.py
+++ survival.py
@@ -1,23 +1,29 @@
 # Import modules
 from game_objects import player
 from game_objects import animal
+import constants
 import dictionary
 
+# Get player character name
+def character_name():
+    character_name = input(constants.MESSAGE_ENTER_NAME)
+    return character_name
+
 # Initialize game objects
-test_player = player.Player("Sally")
-moophant = animal.Animal("Moophant")
+player = player.Player(character_name())
+animal_1 = animal.Animal(constants.ANIMAL_1)
 
 
-# Test simplified gameloop
 def player_survives():
-    action_animal = moophant.get_attack_random()
-    action_player = test_player.set_action_player()
+    action_animal = animal_1.get_attack_random()
+    action_player = player.set_action_player()
 
     if dictionary.attack_response[action_animal] == action_player:
         player_survives = True
+        print(constants.MESSAGE_PLAYER_SURVIVED_TRUE)
     else:
         player_survives = False
-    print("Player survived is", player_survives)
+        print(constants.MESSAGE_PLAYER_SURVIVED_FALSE)
     return player_survives
 
 
