8a64f2e889f9726ade6ca3dab394bf2ef977c2d5
magikitty <elina.satola@gmail.com>
2020-02-20 13:34:04 +0200

    Add get_attack_random() to Animal class

--- game_objects/animal.py
+++ game_objects/animal.py
@@ -1,4 +1,15 @@
+import random
+
 # Class for the mutant animal game object
 class Animal(object):
     def __init__(self, name):
         self.name = name
+        self.attacks = ["stomp", "charge"]
+    def get_attack_random(self):
+        attack_random = random.choice(self.attacks)
+        print(attack_random)
+        return attack_random
+
+
+moophant = Animal("Moophant")
+moophant.get_attack_random()
