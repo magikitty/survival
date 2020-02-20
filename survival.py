1afa14403afb6d89409e8a74e4c4fc6fcfc1172c
magikitty <elina.satola@gmail.com>
2020-02-20 15:17:18 +0200

    Create player_survives() in survival.py to test basic game mechanics and classes

--- survival.py
+++ survival.py
@@ -1,1 +1,25 @@
-# Survival game
+# Import modules
+from game_objects import player
+from game_objects import animal
+
+# Initialize game objects
+test_player = player.Player("Sally")
+moophant = animal.Animal("Moophant")
+
+
+# Test simplified gameloop
+def player_survives():
+    action_animal = moophant.get_attack_random()
+    action_player = test_player.set_action_player()
+
+    if action_animal == "stomp" and action_player == "1":
+        player_survives = True
+    elif action_animal == "charge" and action_player == "2":
+        player_survives = True
+    else:
+        player_survives = False
+    print("Player survived is", player_survives)
+    return player_survives
+
+
+player_survives()
