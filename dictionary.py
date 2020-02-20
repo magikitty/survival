9968174605b5dcc79f3dcc7538d368f23d0d9ff5
magikitty <elina.satola@gmail.com>
2020-02-20 21:08:32 +0200

    Reformat dictionary

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,6 @@
 # Dictionary with animal actions (key) and player response (value) that will surive the encounter
 
-attack_response = {"stomp": "roll", "charge": "hide"}
+attack_response = {
+    "stomp": "roll",
+    "charge": "hide"
+    }
