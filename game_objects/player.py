624b9b00e608dfb7fe90ca8727f64082f40a800d
magikitty <elina.satola@gmail.com>
2020-02-21 12:21:57 +0200

    Create get_input_action(), call in set_action_player()

--- game_objects/player.py
+++ game_objects/player.py
@@ -1,3 +1,6 @@
+import constants
+
+
 # Class for the player game object
 class Player(object):
     def __init__(self, name):
@@ -8,10 +11,19 @@ class Player(object):
 
 
 def set_action_player(Player):
-    action = input("Press 1 or 2: ")
+    action = get_input_action()
     if action == "1":
         print("You roll")
         return Player.actions[0]
     if action == "2":
         print("You hide")
         return Player.actions[1]
+
+
+def get_input_action():
+    print(constants.MENU_PLAYER_ACTIONS)
+    input_valid = False
+    while input_valid == False:
+        input_action = input(constants.MESSAGE_INPUT_ACTION)
+        if input_action == "1" or input_action == "2":
+            return input_action
