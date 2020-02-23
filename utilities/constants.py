823841c945fc43089da8d8608835c09206adc816
magikitty <elina.satola@gmail.com>
2020-02-23 19:56:31 +0200

    Create utilities directory, move constants.py and dictionary.py into utilities

---
+++ utilities/constants.py
@@ -0,0 +1,27 @@
+# Messages
+MESSAGE_ANIMAL_ATTACK = "The mutant animal attacks with"
+MESSAGE_ENCOUNTER_ANIMAL_NAME = "approaches! You have encountered a mutant animal!"
+MESSAGE_ENTER_NAME = "Enter your name: "
+MESSAGE_INPUT_ACTION = "What do you want to do? "
+MESSAGE_PLAYER_SURVIVED_TRUE = "You survived the mutant animal attack!"
+MESSAGE_PLAYER_SURVIVED_FALSE = "Oh no, you did not survive the mutant animal attack!"
+MESSAGE_SET_ACTION = "You decide to"
+
+# Player response actions
+ACTION_1 = "Roll"
+ACTION_2 = "Hide"
+
+# Player actions menu
+MENU_PLAYER_ACTIONS = (
+    "\nPress the number of the response you want to take.\n" +
+    "1. " + ACTION_1 + "\n"
+    "2. " + ACTION_2
+)
+
+# Mutant animal names
+ANIMAL_1 = "Moophant"
+ANIMAL_2 = "Hipposaurus"
+
+# Mutant animal attacks
+ATTACK_1 = "Charge"
+ATTACK_2 = "Stomp"
---
+++ utilities/dictionary.py
@@ -0,0 +1,7 @@
+import utilities.constants
+
+# Dictionary with animal actions (key) and player response (value) that will surive the encounter
+attack_response = {
+    utilities.constants.ATTACK_1: utilities.constants.ACTION_1,
+    utilities.constants.ATTACK_2: utilities.constants.ACTION_2
+    }
