5dc69fafa24510398925b7d9df2a3ae8662db440
magikitty <elina.satola@gmail.com>
2020-02-23 20:07:48 +0200

    Modify Player class and fix import statement

--- game_objects/player.py
+++ game_objects/player.py
@@ -1,15 +1,12 @@
-import constants
-
+from utilities import constants
 
 # Class for the player game object
 class Player(object):
-    def __init__(self, name):
-        self.name = name
-        self.actions = [constants.ACTION_1, constants.ACTION_2]
-    def action(self):
-        return set_action_player(self)
+    name = ""
+    action = "defend"   # debugging
 
 
+# TODO: move
 def set_action_player(Player):
     action = get_input_action()
     if action == "1":
