50ffea094ae49709ea4973d4f4dd79632036ccf8
magikitty <elina.satola@gmail.com>
2020-02-23 20:14:01 +0200

    Delete files moved in commit 823841c945fc43089da8d8608835c09206adc816

--- dictionary.py
+++
@@ -1,7 +0,0 @@
-import constants
-
-# Dictionary with animal actions (key) and player response (value) that will surive the encounter
-attack_response = {
-    constants.ATTACK_1: constants.ACTION_1,
-    constants.ATTACK_2: constants.ACTION_2
-    }
