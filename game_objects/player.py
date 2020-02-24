022a8b1e87655b65e0d35fed72d4f0f0f79e2e55
magikitty <elina.satola@gmail.com>
2020-02-24 12:53:46 +0200

    Create player_action.py, move set_action_player() and get_input_action() from player.py to player_action.py

--- game_objects/player.py
+++ game_objects/player.py
@@ -4,23 +4,3 @@ import utilities.constants as constants
 class Player(object):
     name = ""
     actions = [constants.ACTION_1, constants.ACTION_2]
-
-
-# TODO: move
-def set_action_player(Player):
-    action = get_input_action()
-    if action == "1":
-        print(constants.MESSAGE_SET_ACTION, constants.ACTION_1)
-        return Player.actions[0]
-    if action == "2":
-        print(constants.MESSAGE_SET_ACTION, constants.ACTION_2)
-        return Player.actions[1]
-
-
-def get_input_action():
-    print(constants.MENU_PLAYER_ACTIONS)
-    input_valid = False
-    while input_valid == False:
-        input_action = input(constants.MESSAGE_INPUT_ACTION)
-        if input_action == "1" or input_action == "2":
-            return input_action
