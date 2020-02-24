c573088968398101dcd00fc2eaa1ea39b5b839cf
magikitty <elina.satola@gmail.com>
2020-02-24 19:13:19 +0200

    Remove playerInputFromMessage(), modify setPlayerNameFromInput()

--- game_objects/player_setup.py
+++ game_objects/player_setup.py
@@ -2,11 +2,6 @@ import utilities.constants as constants
 import game_objects.player as player
 
 
-# print given message, return player input
-def playerInputFromMessage(message):
-    return input(message)
-
-
 def setPlayerNameFromInput():
-    name = playerInputFromMessage(constants.MESSAGE_ENTER_NAME)
+    name = input(constants.MESSAGE_ENTER_NAME)
     player.Player.name = name
