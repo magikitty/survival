cff02e7da393f07b13ef427b51542890e97c4e6c
magikitty <elina.satola@gmail.com>
2020-02-24 14:48:47 +0200

    Create welcomePlayer() and gameLoop() in main.py

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
 # Import modules
 import game_objects.animal as animal
 import game_objects.player as player
+import game_objects.player_action as player_action
 import game_objects.player_setup as player_setup
 import utilities.constants as constants
 import utilities.dictionary as dictionary
 
+
+# Get player name and greet player
+def welcomePlayer():
+    print(constants.MESSAGE_WELCOME)
+    player_setup.setPlayerNameFromInput()
+    print("Hello,", player.Player().name + "! Get ready to play!\n")
 
 
 # Initialize animal object
 animal_1 = animal.Animal(constants.ANIMAL_1, constants.ATTACK_1)
 
 
-# def player_survives():
-#     action_animal = animal_1.get_attack()
-#     print(constants.MESSAGE_ANIMAL_ATTACK, action_animal)
-#     action_player = player.Player().actions
+def gameLoop():
+    player_is_alive = True
+    while player_is_alive == True:
+        # Get animal for encounter
+        # Inform player of instantiated animal
+        player_survives_encounter = player_survives()
 
-#     if dictionary.attack_response[action_animal] == action_player:
-#         player_survives = True
-#         print(constants.MESSAGE_PLAYER_SURVIVED_TRUE)
-#     else:
-#         player_survives = False
-#         print(constants.MESSAGE_PLAYER_SURVIVED_FALSE)
-#     return player_survives
+        if player_survives_encounter == False:
+            player_is_alive = False
+            print(constants.MESSAGE_GAME_OVER)
+        else:
+            print(constants.MESSAGE_ANOTHER_ANIMAL_APPROACHES)
 
 
+def player_survives():
+    action_animal = animal_1.get_attack()
+    print(constants.MESSAGE_ANIMAL_ATTACK, action_animal)
+    action_player = player_action.set_action_player(player.Player())
 
-player_setup.setPlayerNameFromInput()
-# player_survives()
-print("player name is", player.Player().name)
-print("player does", player.Player().actions[0])
+    if dictionary.attack_response[action_animal] == action_player:
+        player_survives = True
+        print(constants.MESSAGE_PLAYER_SURVIVED_TRUE)
+    else:
+        player_survives = False
+        print(constants.MESSAGE_PLAYER_SURVIVED_FALSE)
+    return player_survives
+
+
+welcomePlayer()
+gameLoop()
--- utilities/constants.py
+++ utilities/constants.py
@@ -1,11 +1,16 @@
-# Messages
-MESSAGE_ANIMAL_ATTACK = "The mutant animal attacks with"
-MESSAGE_ENCOUNTER_ANIMAL_NAME = "approaches! You have encountered a mutant animal!"
+# Player input messages
 MESSAGE_ENTER_NAME = "Enter your name: "
 MESSAGE_INPUT_ACTION = "What do you want to do? "
-MESSAGE_PLAYER_SURVIVED_TRUE = "You survived the mutant animal attack!"
-MESSAGE_PLAYER_SURVIVED_FALSE = "Oh no, you did not survive the mutant animal attack!"
+
+# Status messages for player
+MESSAGE_ANIMAL_ATTACK = "The mutant animal attacks with"
+MESSAGE_ENCOUNTER_ANIMAL_NAME = "approaches! You have encountered a mutant animal!"
+MESSAGE_GAME_OVER = "Game over!"
+MESSAGE_PLAYER_SURVIVED_TRUE = "You survived the mutant animal attack!\n"
+MESSAGE_PLAYER_SURVIVED_FALSE = "Oh no, you did not survive the mutant animal attack!\n"
 MESSAGE_SET_ACTION = "You decide to"
+MESSAGE_WELCOME = "Welcome to a game of Survival! Try to survive the mutant animal encounters!\n"
+MESSAGE_ANOTHER_ANIMAL_APPROACHES = "But another animal approaches...\n"
 
 # Player response actions
 ACTION_1 = "Roll"
