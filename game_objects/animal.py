a8ef60fb37084e5edc7d8273f9713fac27859702
magikitty <elina.satola@gmail.com>
2020-02-24 19:11:35 +0200

    Modify Animal class, add random attack selection for animal

--- game_objects/animal.py
+++ game_objects/animal.py
@@ -1,21 +1,13 @@
 import random
 import utilities.constants as constants
 
+animal_attacks = [constants.ATTACK_1, constants.ATTACK_2]   # debugging
 
-# Class for the mutant animal game object
+
 class Animal(object):
-    def __init__(self, name, attack):
+    def __init__(self, name):
         self.name = name
-        self.attack = attack
-    def get_attack(self):
-        return self.attack
-
-
-# class Animal(object):
-#     def __init__(self, name):
-#         self.name = name
-#         self.attacks = ["stomp", "charge"]
-#     def get_attack_random(self):
-#         attack_random = random.choice(self.attacks)
-#         print(constants.MESSAGE_ANIMAL_ATTACK, attack_random)
-#         return attack_random
+        self.attacks = animal_attacks
+    def get_attack_random(self):
+        attack_random = random.choice(self.attacks)
+        return attack_random
