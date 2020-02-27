01172335b7a4888f6f4fb089da9aa3d8cb7d3303
magikitty <elina.satola@gmail.com>
2020-02-27 14:54:31 +0200

    Add more animal names and attacks

--- utilities/dictionary.py
+++ utilities/dictionary.py
@@ -5,15 +5,20 @@ attack_response = {
     constants.ATTACK_1: constants.ACTION_1,
     constants.ATTACK_2: constants.ACTION_2,
     constants.ATTACK_3: constants.ACTION_3,
+    constants.ATTACK_4: constants.ACTION_1,
+    constants.ATTACK_5: constants.ACTION_5,
     constants.ATTACK_CLAWS_1: constants.ACTION_5,
     constants.ATTACK_CLAWS_2: constants.ACTION_1,
     constants.ATTACK_CLAWS_3: constants.ACTION_5,
+    constants.ATTACK_CLAWS_4: constants.ACTION_5,
     constants.ATTACK_FLYING_1: constants.ACTION_4,
     constants.ATTACK_FLYING_2: constants.ACTION_5,
     constants.ATTACK_FLYING_3: constants.ACTION_5,
+    constants.ATTACK_FLYING_4: constants.ACTION_2,
     constants.ATTACK_WATER_1: constants.ACTION_4,
     constants.ATTACK_WATER_2: constants.ACTION_2,
-    constants.ATTACK_WATER_3: constants.ACTION_1
+    constants.ATTACK_WATER_3: constants.ACTION_1,
+    constants.ATTACK_WATER_4: constants.ACTION_4
     }
 
 player_input_action = {
@@ -22,4 +27,4 @@ player_input_action = {
     constants.PC_INPUT_3: constants.ACTION_3,
     constants.PC_INPUT_4: constants.ACTION_4,
     constants.PC_INPUT_5: constants.ACTION_5
-}
+    }
--- utilities/lists.py
+++ utilities/lists.py
@@ -1,16 +1,58 @@
 import utilities.constants as constants
 
 # Animal lists
-list_animals = [constants.ANIMAL_1, constants.ANIMAL_2, constants.ANIMAL_3]
-list_animals_claws = [constants.ANIMAL_CLAWS_1, constants.ANIMAL_CLAWS_2, constants.ANIMAL_CLAWS_3]
-list_animals_flying = [constants.ANIMAL_FLYING_1, constants.ANIMAL_FLYING_2, constants.ANIMAL_FLYING_3]
-list_animals_water = [constants.ANIMAL_WATER_1, constants.ANIMAL_WATER_2, constants.ANIMAL_WATER_3]
+list_animals = [
+    constants.ANIMAL_1, 
+    constants.ANIMAL_2, 
+    constants.ANIMAL_3, 
+    constants.ANIMAL_4, 
+    constants.ANIMAL_5]
+
+list_animals_claws = [
+    constants.ANIMAL_CLAWS_1, 
+    constants.ANIMAL_CLAWS_2, 
+    constants.ANIMAL_CLAWS_3]
+
+list_animals_flying = [
+    constants.ANIMAL_FLYING_1, 
+    constants.ANIMAL_FLYING_2, 
+    constants.ANIMAL_FLYING_3]
+
+list_animals_water = [
+    constants.ANIMAL_WATER_1, 
+    constants.ANIMAL_WATER_2, 
+    constants.ANIMAL_WATER_3]
 
 # Animal attack lists
-animal_attacks = [constants.ATTACK_1, constants.ATTACK_2, constants.ATTACK_3]
-animal_attacks_claws = [constants.ATTACK_CLAWS_1, constants.ATTACK_CLAWS_2, constants.ATTACK_CLAWS_3]
-animal_attacks_flying = [constants.ATTACK_FLYING_1, constants.ATTACK_FLYING_2, constants.ATTACK_FLYING_3]
-animal_attacks_water = [constants.ATTACK_WATER_1, constants.ATTACK_WATER_2, constants.ATTACK_WATER_3]
+animal_attacks = [
+    constants.ATTACK_1, 
+    constants.ATTACK_2, 
+    constants.ATTACK_3, 
+    constants.ATTACK_4,
+    constants.ATTACK_5]
+
+animal_attacks_claws = [
+    constants.ATTACK_CLAWS_1, 
+    constants.ATTACK_CLAWS_2, 
+    constants.ATTACK_CLAWS_3, 
+    constants.ATTACK_CLAWS_4]
+
+animal_attacks_flying = [
+    constants.ATTACK_FLYING_1, 
+    constants.ATTACK_FLYING_2, 
+    constants.ATTACK_FLYING_3, 
+    constants.ATTACK_FLYING_4]
+
+animal_attacks_water = [
+    constants.ATTACK_WATER_1, 
+    constants.ATTACK_WATER_2, 
+    constants.ATTACK_WATER_3, 
+    constants.ATTACK_WATER_4]
 
 # PC lists
-pc_actions = [constants.ACTION_1, constants.ACTION_2, constants.ACTION_3, constants.ACTION_4, constants.ACTION_5]
+pc_actions = [
+    constants.ACTION_1,
+    constants.ACTION_2,
+    constants.ACTION_3,
+    constants.ACTION_4,
+    constants.ACTION_5]
